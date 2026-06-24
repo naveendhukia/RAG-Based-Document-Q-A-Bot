b1ad730bb2cde3a6706b7a4096b4d867c5a1db0a
naveendhukia <24B1832@iitb.ac.in>
2026-06-24 12:14:39 +0530

    Update rag_engine.py

--- backend/rag_engine.py
+++ backend/rag_engine.py
@@ -1,2 +1,31 @@
 import os
-import genai from gemini
+import chromadb
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+client = genai.Client()
+
+def text_response(content):
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = content
+    )
+    return response.text
+
+def embedding_response(chunks):
+    response = client.models.generate_content(
+        model = "text-embedding-004",
+        contents = chunks
+    )
+    return response.values
+
+def add_to_chromadb(collection, embedding, pdf_path,chunk_id ):
+    collection.add(
+        embeddings = [embedding],
+        source_data = [{"source":pdf_path,
+                        "chunk_id":chunk_id}]
+    )
+    return collection
+
+
